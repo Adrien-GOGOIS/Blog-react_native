47a6425248730401d9caf5fca94f4319d7a647cc
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-03 12:01:57 +0100

    Possibility to add a post in app context

--- views/AddPost.js
+++ views/AddPost.js
@@ -1,9 +1,65 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+// Imports react
+import { useContext, useState } from "react";
+
+// Context
+import { UserContext } from "../App";
 
 export default function AddPost() {
+  // State du context
+  const userState = useContext(UserContext);
+
+  // State du post dans l'input
+  const [textInput, setTextInput] = useState([]);
+
+  // Fonction récupération du texte de l'utilisateur dans l'input
+  //   const handleChange = (e) => {
+  //     setTextInput([e.target.value]);
+  //   };
+
+  // Fonction de push du post dans le context de l'app pour réutilisation dans Timeline
+  const handleSubmit = () => {
+    userState.setUserPost(...userState.userPost, [textInput]);
+  };
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>AddPost</Text>
-    </View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Ecrivez ce qui vous passe par la tête"
+          onChangeText={setTextInput}
+        />
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    borderColor: "black",
+  },
+
+  button: {
+    borderColor: "black",
+  },
+});
